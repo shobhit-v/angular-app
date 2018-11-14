90bc0bbcbcdac106175f26dd1daa4d0b5433db38
shobhit verma <shobhitverma88@gmail.com>
2018-11-14 21:33:58 +0530

    Reactive form with sigal filed form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ReactiveFormComponent implements OnInit {
 
   constructor() { }
+  testControl = new FormControl('Hello');
 
   ngOnInit() {
   }
