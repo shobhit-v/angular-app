3931510ff50919aa2c8b5e1dd37afb86fdd7a723
Shobhit Verma <shobhit@acchajeemail.com>
2018-11-16 18:19:55 +0530

    Reactive-Form-FormBuilder

--- src/app/reactive-multiple-form/reactive-multiple-form.component.ts
+++ src/app/reactive-multiple-form/reactive-multiple-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-multiple-form',
@@ -8,15 +8,33 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class ReactiveMultipleFormComponent implements OnInit {
 
-  constructor() { }
+  loginForm;
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: [{ value: 'Abc@gmail.com', disabled: true }, [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      address: this.fb.group({
+        addressLine1: '',
+        addressLine2: '',
+        city: '',
+        state: ''
+      })
+    })
   }
 
-  loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', Validators.required)
-  });
+  submitForm() {
+    // console.log(this.loginForm.value)
+    console.log(this.loginForm.getRawValue())
+  }
+
+  // loginForm = new FormGroup({
+  //   email: new FormControl('', [Validators.required, Validators.email]),
+  //   password: new FormControl('', Validators.required)
+  // });
 
   get emailControl() {
     return this.loginForm.get('email')
